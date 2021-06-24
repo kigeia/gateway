2e8000828d311ecd0d4f0efac75b67f466b746b3
Josselin Jacquard <josselin.jacquard@gmail.com>
2021-06-24 19:11:30 +0200

    add lydia connector

---
+++ src/routes/lydia.route.js
@@ -0,0 +1,14 @@
+import { ethers } from 'ethers';
+
+import Uniswap from '../services/uniswap';
+import getRouter from './generic.uniswap.route';
+import Ethereum from '../services/eth';
+const eth = new Ethereum(process.env.ETHEREUM_CHAIN)
+
+var uni = require('@olive-dev/avax-sdk')
+var chainID = 43114
+const uniswap = new Uniswap(chainID, uni, eth.spenders['olive'])
+
+const router = getRouter(uniswap)
+
+export default router;
